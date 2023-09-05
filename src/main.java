dd691737f25dcd034d560367acfc7b0a3567f503
Karpovich <karrrrp@yandex.ru>
2023-09-05 11:05:24 +0300

    Changes

--- src/main.java
+++ src/main.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class main {
+public class main {
+    public static void main(String[] args) {
+        int start_act = 100;
+        int top_up_sum = 1300;
+        System.out.println ("BALANCE");
+        if (top_up_sum > 1000) {
+            System.out.println (start_act + top_up_sum / 100 + top_up_sum);
+        } else {
+            System.out.println (start_act+top_up_sum);}
+    }
 }
